0fe484c194054b2df14eb3b028d7c9fbbf791767
Varsha <varshamrao69@gmail.com>
2026-05-04 21:22:52 +0530

    My gittignore commit

---
+++ dary.java
@@ -0,0 +1,18 @@
+
+package demo;
+
+/**
+ *
+ * @author ise
+ */
+public class Demo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+       
+    }
+    
+}
